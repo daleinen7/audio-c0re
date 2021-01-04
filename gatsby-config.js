4b5b88be8250a8c2eaa1e5103f9f7532a3241f70
daleinen7 <daleinen@gmail.com>
2021-01-04 13:34:14 -0600

    Add environmental variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,35 @@
+require('dotenv').config();
+
+module.exports = {
+  siteMetadata: {
+    title: "AudioC0RE",
+    siteUrl: "http://audioc0re.com"
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-transformer-sharp",
+    `gatsby-transformer-remark`,
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
+
+
 module.exports = {
   siteMetadata: {
     title: "AudioC0RE",
