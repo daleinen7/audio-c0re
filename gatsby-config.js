43ac72d1859d58968c26e3ec31453994ec0324d9
daleinen7 <daleinen@gmail.com>
2021-01-04 14:07:46 -0600

    fix mistake of doubling config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,31 +28,3 @@ module.exports = {
     },
   ],
 };
-
-
-module.exports = {
-  siteMetadata: {
-    title: "AudioC0RE",
-  },
-  plugins: [
-    {
-      resolve: "gatsby-source-contentful",
-      options: {
-        accessToken: "JL30ysp4sIqQZSh2VPfl1KoRpDhlnTHIha6-jr5dh3s",
-        spaceId: "JL30ysp4sIqQZSh2VPfl1KoRpDhlnTHIha6-jr5dh3s",
-      },
-    },
-    "gatsby-plugin-sharp",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-  ],
-};
