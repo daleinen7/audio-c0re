22cd5d1b7016a039fc227a27aacd81d0f942dc16
daleinen7 <daleinen@gmail.com>
2021-01-04 10:58:39 -0600

    alter readme and index from defeault templates

---
+++ src/pages/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return(
+    <div>
+      <h1>AudioC0RE</h1>
+      <p>like ridesharing but with headphones?</p>
+    </div>
+  )
+}
